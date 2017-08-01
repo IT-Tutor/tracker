08e66dc6d9d46a5e9756b5496168a6cd05c28fa3
Graunephar <daniel@graungaard.com>
2017-08-01 22:34:48 +0200

    added tracking and dashboard

--- tracker.py
+++ tracker.py
@@ -1,13 +1,51 @@
-from flask import Flask
+import datetime
+
+import flask
+from flask import Flask, session
+
+
+class number(object):
+    def __init__(self):
+        self.number = 0
+
+    def increment(self):
+        self.number += 1
+
+    def get_number(self):
+        return self.number
 
 app = Flask(__name__)
+visitors = {}
+init_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
+
+totalvisits = number()
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def dashboard():
 
+    print(init_time)
+    unique_visitors = len(visitors)
+    total = totalvisits.get_number()
+
+    return flask.render_template('dashboard.html', totalvisits=total, unique_visitors=unique_visitors, init_time=init_time)
+
+
+@app.route('/track')
+def track():
+    totalvisits.increment()
+    ip = flask.request.remote_addr
+    if ip in visitors:
+        nr = visitors[ip]
+        visitors[ip] = nr + 1
+        print("GAMMEL")
+        print(nr)
+    else:
+        visitors[ip] = 0
+        print("NY")
+
+    return "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" #base 64 encoded 1x1px gif
 
 if __name__ == '__main__':
     app.run()
